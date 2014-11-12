92e7958867b20c7d4f1fbbb284be2ef02e0193b5
ahfergus1 <ahfergus1@gmail.com>
2014-11-12 14:17:31 -0500

    Added idea files (CLion stuff) to the .gitignore.  Updated CMakeLists.txt.  Added header and main to robot.cpp.

---
+++ asn2/robot.cpp
@@ -0,0 +1,18 @@
+/*
+    Robot - Observer Design Pattern Demonstrator
+
+    Created by Andrew Simpson on 2014-11-11
+
+    Written by Andrew Simpson and Dustin Dobransky
+
+    This is the main file of
+ */
+
+#include <stdio.h>
+
+/*
+    The main function of our program.
+ */
+int main(){
+    puts("Hello world!");
+}
