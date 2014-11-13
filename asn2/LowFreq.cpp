4ab60651659381f9a7cf573753d883d250a6e0ec
ahfergus1 <ahfergus1@gmail.com>
2014-11-13 17:30:18 -0500

    Added comments and more logging.

--- asn2/LowFreq.cpp
+++ asn2/LowFreq.cpp
@@ -1,6 +1,13 @@
 #include "LowFreq.h"
 #include <syslog.h>
 
+
+/*
+    Constructor
+
+    Set initial state to zero - it will disappear after
+    a sufficient number of readings.
+ */
 LowFreq::LowFreq(){
     syslog(LOG_DEBUG, "Entered LowFreq::LowFreq()");
     state = 0;
@@ -8,8 +15,26 @@ LowFreq::LowFreq(){
     return;
 }
 
-LowFreq::~LowFreq(){}
+/*
+    Destructor
 
+    Nothing special to do - leave the body empty.
+ */
+LowFreq::~LowFreq(){
+    syslog(LOG_DEBUG, "Entered LowestVal::~LowestVal()");
+    syslog(LOG_DEBUG, "Exited LowestVal::~LowestVal()");
+    return;
+}
+
+/*
+    update
+
+    Function called by the subject's notify function to let the
+    observer know that the state has changed.
+
+    Take the weighted average of the last state and the update
+    reading, then save that as the current state.
+ */
 void LowFreq::update(float updateVal){
     syslog(LOG_DEBUG, "Entered LowFreq::update()");
     state = state*0.75 + updateVal*0.25;
@@ -18,6 +43,11 @@ void LowFreq::update(float updateVal){
     return;
 }
 
+/*
+    getState
+
+    Just returns the current state to the caller.
+ */
 float LowFreq::getState(){
     syslog(LOG_DEBUG, "Entered LowFreq::getState()");
     syslog(LOG_DEBUG, "Exited LowFreq::getState()");
--- asn2/LowestVal.cpp
+++ asn2/LowestVal.cpp
@@ -2,6 +2,12 @@
 #include <syslog.h>
 #include <math.h>
 
+/*
+    Constructor
+
+    Set initial state to inifinity - that way the first reading
+    will always become the lowest reading.
+ */
 LowestVal::LowestVal(){
     syslog(LOG_DEBUG, "Entered LowestVal::LowestVal()");
     state = infinity();
@@ -9,8 +15,25 @@ LowestVal::LowestVal(){
     return;
 }
 
-LowestVal::~LowestVal(){}
+/*
+    Destructor
 
+    Nothing special to do - just log destruction.
+ */
+LowestVal::~LowestVal(){
+    syslog(LOG_DEBUG, "Entered LowestVal::~LowestVal()");
+    syslog(LOG_DEBUG, "Exited LowestVal::~LowestVal()");
+    return;
+}
+
+/*
+    update
+
+    Function called by the subject's notify function to let the
+    observer know that the state has changed.
+
+    Save the updated value if it is less than the stored one.
+ */
 void LowestVal::update(float updateVal){
     syslog(LOG_DEBUG, "Entered LowestVal::update()");
     if (updateVal < state){
@@ -21,6 +44,11 @@ void LowestVal::update(float updateVal){
     return;
 }
 
+/*
+    getState
+
+    Just returns the current state to the caller.
+ */
 float LowestVal::getState(){
     syslog(LOG_DEBUG, "Entered LowestVal::getState()");
     syslog(LOG_DEBUG, "Exited LowestVal::getState()");
