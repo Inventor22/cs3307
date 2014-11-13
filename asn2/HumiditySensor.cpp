f55186d7d77ec26ab57f60ca787fc64daf46d927
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2014-11-13 15:16:56 -0500

    added comments

--- asn2/HumiditySensor.cpp
+++ asn2/HumiditySensor.cpp
@@ -1,15 +1,44 @@
+/* Name: Dustin Dobransky
+ID: 250575030
+Date: 12/11/14
+
+Concrete Sensor definition, CS3307 Assignment 2
+Specifically, a humidity sensor.
+
+Description:
+This is a declaration for the humidity sensor class, a subclass of Sensor.
+
+The sensor should return the relative humidity of the soil from [0, 1],
+no moisture to 100% humidity, respectively.
+
+In this skeleton class, a value of 1 is always returned.
+
+Each time the sensor calls the getHumidity() function, the subclass' notify function is also called.
+The notify function will update the observers (the 'algorithms') with this new value.
+*/
 #include <sys/syslog.h>
 #include "HumiditySensor.h"
 
+/// <summary>
+/// Initializes a new instance of the <see cref="HumiditySensor"/> class.
+/// </summary>
 HumiditySensor::HumiditySensor() {
     syslog(LOG_DEBUG, "Entered HumiditySensor()");
 
 }
 
+/// <summary>
+/// Finalizes an instance of the <see cref="HumiditySensor"/> class.
+/// </summary>
 HumiditySensor::~HumiditySensor() {
     syslog(LOG_DEBUG, "Entered ~HumiditySensor()");
 }
 
+/// <summary>
+/// Gets the humidity.
+/// Also calls the notify function, from the super class.
+/// </summary>
+/// <returns>the humidity</returns>
 int HumiditySensor::getHumidity() {
     syslog(LOG_DEBUG, "Entered getHumidity()");
     int hum = 1;
--- asn2/sensor.h
+++ asn2/sensor.h
@@ -1,3 +1,19 @@
+/* Name: Dustin Dobransky
+ID: 250575030
+Date: 12/11/14
+
+Sensor declaration, CS3307 Assignment 2
+
+Description:
+This is the declaration for the base sensor class.
+The observer pattern is used:  For every sensor, there is an
+associated list of algorithms which run.
+Each concrete sensor class (sub class of Sensor) makes a call to
+method 'notify' when the sensor performs a new reading.  Therefore,
+all attached algorithms are immediately updated with the newest sensor data
+as soon as its collected.
+*/
+
 #ifndef SENSOR_H
 #define SENSOR_H
 
@@ -5,14 +21,21 @@
 #include "Algorithm.h"
 
 class Sensor {
-private:
+private:    
+    /// <summary>
+    /// The vector to hold the (pointers to) algorithms which are updated with a call to notify
+    /// The various algorithms operate on the sensor.  For example, a sensor may have a
+    /// low pass filter algorithm attached.  Or, multiple algorithms.  One could be an averaging filter,
+    /// another could be a low/high pass filter, or notch filter.
+    /// </summary>
     std::vector<Algorithm*> algorithms;
 
-public:
-    Sensor();
+public:    
+    Sensor();    
     ~Sensor();
-    void attach(Algorithm* algo);
-    void detach(Algorithm* algo);
+    
+    void attach(Algorithm* algo);    
+    void detach(Algorithm* algo);    
     void notify(float a);
 };
 
