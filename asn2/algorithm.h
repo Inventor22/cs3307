87ab315aa7b85691f9690aeaed807c434abe791c
ahfergus1 <ahfergus1@gmail.com>
2014-11-12 20:38:58 -0500

    Added updated Visio UML

---
+++ asn2/algorithm.h
@@ -0,0 +1,16 @@
+/*
+    Algorithm - Our Observer Class
+
+    Created by Andrew Simpson on 2014-11-11
+
+    Written by Andrew Simpson
+
+
+ */
+
+class Algorithm {
+protected:
+
+public:
+    void Update();
+};
--- asn2/robot.cpp
+++ asn2/robot.cpp
@@ -8,11 +8,45 @@
     This is the main file of
  */
 
-#include <stdio.h>
+#include <syslog.h>
+#include <stdlib.h>
 
 /*
     The main function of our program.
  */
-int main(){
-    puts("Hello world!");
+int main(int argc, char** argv){
+
+    //Start logging
+    openlog("robot_tester", LOG_PERROR | LOG_PID | LOG_NDELAY, <#(int)#>LOG_USER);
+    int log_level = LOG_DEBUG;
+    setlogmask(log_level);
+
+    //Arguments are unused for now
+    if(argc < 0){
+        syslog(LOG_ERR, "Too few arguments!");
+    }
+
+    //Create some sensors
+
+
+    //Create some algorithms
+
+
+    //Attach senors to algorithms
+
+
+    //Do some updates
+
+
+    //Kill an algorithm
+
+
+    //Kill a sensor
+
+
+    //End logging
+    closelog();
+
+    //End
+    return EXIT_SUCCESS;
 }
