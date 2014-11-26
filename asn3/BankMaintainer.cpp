eeb900cfc50242055d7204362df93428b4e8b6c1
bobAtk <stempower@gmail.com>
2014-11-26 18:07:01 -0500

    Enum Fix BankMaintainer.cpp
    Fixes enum issue for GAUL...

--- asn3/BankMaintainer.cpp
+++ asn3/BankMaintainer.cpp
@@ -4,11 +4,11 @@ BankMaintainer::BankMaintainer(std::ifstream& i) : BankMember(i) {
 }
 
 BankMaintainer::BankMaintainer(std::string fN, std::string lN, unsigned int pin) :
-BankMember(fN, lN, pin, MemberType::MAINTENANCE) {
+BankMember(fN, lN, pin, MAINTENANCE) {
 }
 
 BankMaintainer::BankMaintainer(std::string fN, std::string lN, unsigned long id, unsigned int pin) :
-BankMember(fN, lN, id, pin, MemberType::MAINTENANCE) {
+BankMember(fN, lN, id, pin, MAINTENANCE) {
 }
 
 //void setTrace(Bank::TraceState traceState)
@@ -31,3 +31,4 @@ void BankMaintainer::write(std::ofstream& o) {
 	o << "Maintainer" << " ";
 	BankMember::writeToFile(o);
 }
+
